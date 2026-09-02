d3584661d8a6afe9cc295cf202e1895fa287a68b
shan011117 <576557292@qq.com>
2026-09-02 21:44:50 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Service Worker - 离线缓存
+const CACHE_NAME = 'gyfj-ledger-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS).catch(() => {
+                return cache.add('./index.html');
+            });
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            const fetchPromise = fetch(event.request).then((response) => {
+                if (response && response.status === 200) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, clone).catch(() => {});
+                    });
+                }
+                return response;
+            }).catch(() => cached);
+            return cached || fetchPromise;
+        })
+    );
+});
